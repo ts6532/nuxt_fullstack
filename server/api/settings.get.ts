b0539fa83dc9e3a901241c825d9339865c0163e7
Dmitry <papkovdmitry@gmail.com>
2025-12-28 11:36:51 +0300

    hot-fix: settings access

--- server/api/settings.get.ts
+++ server/api/settings.get.ts
@@ -3,13 +3,27 @@ import { FileDTO } from "~~/server/models/file";
 import SettingsModel from "~~/server/models/settings";
 
 export default defineEventHandler(async () => {
+  let settings;
+
   try {
-    let settings = await SettingsModel.findOne();
+    settings = await SettingsModel.findOne();
+  } catch (error) {
+    console.log("No settings", error);
+  }
 
-    if (!settings) {
-      await SettingsModel.create({});
-    }
+  try {
+    console.log("Creating Settings");
+    settings = new SettingsModel({});
+    await settings.save()
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Ошибка при создании настроек",
+      data: error,
+    });
+  }
 
+  try {
     let populatedSettings = await SettingsModel.findOne()
       .populate<{ heroImage: FileDTO }>("heroImage")
       .populate<{ aboutImage: FileDTO }>("aboutImage")
